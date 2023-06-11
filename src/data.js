0a337314ebf0c97863378a7655f9de7fcf793333
Jakub Strózik <jakub.strozik@wp.pl>
2023-06-11 11:22:56 +0200

    data + menu

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        id: 1,
+        name: testname,
+        description: "jakis opis np: jakis slimak tu byl",
+        price: "199,99zł",
+    }
+
+    {
+        id: 2,
+        name: testname2,
+        description: "jakis opis np: jakis slimak tu byl",
+        price: "69,99zł",
+    }
+
+]
